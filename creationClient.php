3790473ce58a6bb3f42ae351802f89e3b0ce5a6e
nils <26379736+NiGour@users.noreply.github.com>
2023-03-27 17:08:24 +0200

    contenu.php now displays the content of the variable contenu that is specific to each php page

--- creationClient.php
+++ creationClient.php
@@ -2,7 +2,7 @@
 //displaying errors
 ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 
-$contenu="page principale, où seront affichées les colonnes de la base de données ";
+$contenu="phrase permettant de tester l'affichage de la variable contenu";
 
 require 'partials/enTete.php';
 require 'partials/contenu.php';
--- partials/contenu.php
+++ partials/contenu.php
@@ -1,3 +1,3 @@
 <main>
-        affichage
-    </main>
+<?=$contenu;?>
+</main>
