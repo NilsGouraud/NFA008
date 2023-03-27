a2e14693acc0072cbe95cecdcd25d8168936a9dd
nils <26379736+NiGour@users.noreply.github.com>
2023-03-27 18:34:41 +0200

    managed to add a second customer with the form

--- ajouter.php
+++ ajouter.php
@@ -3,31 +3,34 @@
 ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 
 print_r($_POST);
-
-$nom=$_POST['nom'];
-$numeroDeVoie=$_POST['numeroDeVoie'];
-$rue=$_POST['rue'];
-$ville=$_POST['ville'];
-$codePostal=$_POST['codePostal'];
-$values="'','$nom','$numeroDeVoie','$rue','$ville','$codePostal'";
+$id=0;
+$nom_client=$_POST['nom_client'];
+$numéro_de_rue_client=$_POST['numéro_de_rue_client'];
+$nom_rue=$_POST['nom_rue'];
+$ville_client=$_POST['ville_client'];
+$code_postal_client=$_POST['code_postal_client'];
+$values="$id,'$nom_client','$numéro_de_rue_client','$nom_rue','$ville_client','$code_postal_client'";
 echo 'connected';
 
 $dsn="mysql:host=localhost;port=3306;dbname=cereale;user=root;password=a;charset=utf8mb4";
+function afficher($string){
+  echo '<pre>';
+  var_dump($string);
+  echo '</pre>';
+}
 afficher($dsn);
-$pdo=new PDO($dsn);
-$statement=$pdo->prepare("select * from client");
-$statement->execute();
-$requete = 'INSERT INTO client(id, nom, numeroDeVoie,rue, ville, codePostal) VALUES($values)';
- // on insère les informations du formulaire dans la table
- $connexion;
- mysqli_query($pdo,$requete);
- mysqli_close($pdo); // on ferme la connexion
+afficher($values);
 
- if ($connexion) {
-    echo 'connected';
-  } else { 
-    echo 'not connected';
-  }
+$pdo=new PDO($dsn);
+$client='client(numéro_client, nom_client, numéro_de_rue_client,nom_rue, ville_client, code_postal_client)';
+$statement=$pdo->prepare('INSERT INTO '.$client.' VALUES('.$values.')');
+//$statement=$pdo->prepare('INSERT INTO client(numéro_client, nom_client, numéro_de_rue_client,nom_rue, ville_client, code_postal_client) VALUES(0,$nom_client,$numéro_de_rue_client,$nom_rue,$ville_client,$code_postal_client)');
+afficher($statement);
+
+$statement->execute();
+
+// on insère les informations du formulaire dans la table
+
 
 /*
 $hote='localhost';  $utilisateur='root';  $password='root'; 
