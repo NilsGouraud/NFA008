60f21f7a7479299aca8f19b189cf0c0a639d2e25
nils <26379736+NiGour@users.noreply.github.com>
2023-03-27 14:34:42 +0200

    created the main html page and its php counterparts

---
+++ contenu.php
@@ -0,0 +1,3 @@
+<main>
+        affichage
+    </main>
---
+++ enTete.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>Projet base de données</header>
+    <nav>
+        <a href="affichageCerales.html">page principale</a>
+        <a href="creationClient.html">ajouter un client</a>
+        <a href="reception.html">réceptionner un lot de  céréales</a>
+    </nav>
+
+    
